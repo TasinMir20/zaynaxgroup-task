e08fdc9e61822aa81c331512d952f4e41bb7d555
Tasin Mir <tasinmir20@gmail.com>
2022-04-11 17:24:14 +0600

    Product search

--- api/controllers/products.js
+++ api/controllers/products.js
@@ -2,13 +2,23 @@ const Product = require("../../models/Product");
 
 exports.products = async (req, res, next) => {
 	try {
-		let { skip, limit } = req.query;
+		let { skip, limit, search } = req.query;
 
 		skip = parseInt(skip, 10) || 0;
 		limit = parseInt(limit, 10) || 20;
 
-		const getProduct = await Product.find({}).sort({ createdAt: -1 }).skip(skip).limit(limit);
-		return res.json({ products: getProduct });
+		if (search) {
+			function es(str) {
+				return str.replace(/[-\/\\^$*+?()|[\]{}]/g, "");
+			}
+			const KeyWordRegExp = new RegExp(".*" + es(search) + ".*", "i");
+
+			const foundProducts = await Product.find({ name: KeyWordRegExp }).sort({ createdAt: -1 }).skip(skip).limit(limit);
+			return res.json({ products: foundProducts });
+		} else {
+			const getProduct = await Product.find({}).sort({ createdAt: -1 }).skip(skip).limit(limit);
+			return res.json({ products: getProduct });
+		}
 	} catch (err) {
 		next(err);
 	}
